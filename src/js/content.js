cc53f576035ed5118705b6412f2e0f5fd229d31b
pythias <pythias@gmail.com>
2021-01-28 00:43:24 +0800

    重复问题处理

--- src/js/content.js
+++ src/js/content.js
@@ -1,5 +1,6 @@
 chrome.storage.local.get(['url', 'tab'], function (data) {
     if (typeof shortenOnTab !== 'undefined' && data.url !== undefined) {
+        chrome.storage.local.clear();
         shortenOnTab(data.url, data.tab);
     }
 });
